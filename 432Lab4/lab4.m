36e02c50efcfbc395b059c9d5de55c771eaa016a
Kevin Chang <kchang98@uw.edu>
2020-05-16 19:43:30 -0700

    refactor initial graphing into a for loop, add comments

--- 432Lab4/lab4.m
+++ 432Lab4/lab4.m
@@ -1,5 +1,15 @@
 close all; clear; clc;
 
+%% Data input
+
+% data is a cell array that contains all the individual data, which each
+% have a TIME and CH1 column where TIME corresponds to x-axis data and CH1
+% corresponds to y-axis data. The names with matching indices are in the
+% names array as a 1D string array. These names can be changed without
+% breaking anything to be more descriptive later, currently they're just
+% the same names as the individual tables that are being read from the
+% csv's.
+
 g_B0 = readtable('Green_B0.csv', 'HeaderLines', 15);
 g_3612_NP = readtable('Green_3612A_no_polarizer.csv', 'HeaderLines', 15);
 g_357A_Sig = readtable('Green_357A_sigma.csv', 'HeaderLines', 15);
@@ -11,33 +21,34 @@ y_380A_Pi = readtable('Yellow_380A_pi.csv', 'HeaderLines', 15);
 y_182A_Sig = readtable('Yellow_182A_sigma.csv', 'HeaderLines', 15);
 y_182A_Pi = readtable('Yellow_182A_pi.csv', 'HeaderLines', 15);
 
-%%
+data = {g_B0, g_3612_NP, g_357A_Sig, g_357A_Pi, y_B0, y_1817_NP, y_380A_Pi, y_182A_Sig, y_182A_Pi};
+names = ["g_B0", "g_3612_NP", "g_357A_Sig", "g_357A_Pi", "y_B0", "y_1817_NP", "y_380A_Pi", "y_182A_Sig", "y_182A_Pi"];
 
-% Green
-figure()
-plot(g_B0.('TIME'), g_B0.('CH1'))
+%% Initial Raw Data Plots
 
-figure()
-plot(g_3612_NP.('TIME'), g_3612_NP.('CH1'))
+% I've got one for loop that iterates through the data object to create
+% each graph.
 
-figure()
-plot(g_357A_Pi.('TIME'), g_357A_Pi.('CH1'))
+% The peaks and their locations are found and compiled into the
+% peakData object, which is a cell array of tables which have the first
+% column being the y-axis value of the peak and the second column being the
+% location on the x-axis (I believe, gotta confirm that).
 
-figure()
-plot(g_357A_Sig.('TIME'), g_357A_Sig.('CH1'))
+% TODO: Refine peak-finding parameters to avoid missed peaks in certain
+% graphs
+% TODO: Probably rename the names array for better graph naming
+% TODO: Possibly label each point on the graph, though might not be needed
+% TODO: Find FWHM
 
-% Yellow
-figure()
-plot(y_B0.('TIME'), y_B0.('CH1'))
+for i = 1:9
+    figure('Name', names(i))
+    hold on
 
-figure()
-plot(y_1817_NP.('TIME'), y_1817_NP.('CH1'))
+    plot(data{i}.('TIME'), data{i}.('CH1'))
 
-figure()
-plot(y_182A_Pi.('TIME'), y_182A_Pi.('CH1'))
+    [Peaks, Locs] = findpeaks(data{1}.('CH1'), data{i}.('TIME'), 'MinPeakProminence', .05, 'MinPeakDistance', .0001);
+    peakData{i} = [Peaks, Locs];
+    findpeaks(data{i}.('CH1'), data{i}.('TIME'), 'MinPeakProminence', .05, 'MinPeakDistance', .0001);
 
-figure()
-plot(y_182A_Sig.('TIME'), y_182A_Sig.('CH1'))
-
-figure()
-plot(y_380A_Pi.('TIME'), y_380A_Pi.('CH1'))
+    hold off
+end
